05fd2cfb663050cf244a7cd0cf0a9aa0a5598bcc
michelif <francesco.micheli@cern.ch>
2018-07-23 11:44:01 +0200

    updating one file

--- python/etc/tnpInputTestFiles_cff.py
+++ python/etc/tnpInputTestFiles_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 
 filesMiniAOD_Preliminary2018 = {
     'mc' :  cms.untracked.vstring(
-        '/store/mc/RunIISpring18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/100X_upgrade2018_realistic_v10-v2/100000/6815ED2D-7530-E811-90C0-FA163E27991E.root',
+#        '/store/mc/RunIISpring18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/100X_upgrade2018_realistic_v10-v2/100000/6815ED2D-7530-E811-90C0-FA163E27991E.root',
+        '/store/mc/RunIISpring18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/100X_upgrade2018_realistic_v10-v2/100000/968B1BAD-332F-E811-8178-FA163E9F7460.root',
         ),
 
     'data' : cms.untracked.vstring(
