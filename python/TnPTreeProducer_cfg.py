b7f4de381ee4324e9398019238b06f03678ea31d
tomcornelis <tomc@cern.ch>
2020-06-09 16:00:17 +0200

    Update GT for 2018

--- python/TnPTreeProducer_cfg.py
+++ python/TnPTreeProducer_cfg.py
@@ -98,13 +98,13 @@ if varOptions.GT == "auto":
   if options['isMC']:
     if options['era'] == '2016':   options['GLOBALTAG'] = '94X_mcRun2_asymptotic_v3'
     if options['era'] == '2017':   options['GLOBALTAG'] = '94X_mc2017_realistic_v17'
-    if options['era'] == '2018':   options['GLOBALTAG'] = '102X_upgrade2018_realistic_v20'
+    if options['era'] == '2018':   options['GLOBALTAG'] = '102X_upgrade2018_realistic_v21'
     if options['era'] == 'UL2017': options['GLOBALTAG'] = '106X_dataRun2_v28'
     if options['era'] == 'UL2018': options['GLOBALTAG'] = '106X_dataRun2_v28'
   else:
     if options['era'] == '2016':   options['GLOBALTAG'] = '94X_dataRun2_v10'
     if options['era'] == '2017':   options['GLOBALTAG'] = '94X_dataRun2_v11'
-    if options['era'] == '2018':   options['GLOBALTAG'] = '102X_dataRun2_v12'
+    if options['era'] == '2018':   options['GLOBALTAG'] = '102X_dataRun2_v13'
     if options['era'] == 'UL2017': options['GLOBALTAG'] = '106X_mc2017_realistic_v7'
     if options['era'] == 'UL2018': options['GLOBALTAG'] = '106X_upgrade2018_realistic_v11_L1v1'
 else:
